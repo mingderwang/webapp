b0f2b61555fa569338cbb08634afcae03e48dafe
Ming-der Wang <mingderwang@gmail.com>
2018-04-05 09:16:55 +0800

    test ok to add many react component in index

--- src/index.js
+++ src/index.js
@@ -4,8 +4,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import Index from './pages/index';
 //import ImageGrid from './pages/imageGrid';
-//import TitleBar from './pages/titleBar';
+import TitleBar from './pages/titleBar';
 import SimpleTable from './pages/simpleTable'
 
 // $FlowIgnore - we don't want the missing dom element to be a silent error.
-ReactDOM.render(<SimpleTable />, document.querySelector('#root'));
+ReactDOM.render(
+  <div class="box">
+  <p>Test</p>
+  <a href="/" class="button">Button</a>
+  <TitleBar/>
+<SimpleTable />
+
+  <style jsx>{`
+    .box {
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+.button {
+  background-color: #174E7D;
+  border-radius: 5px;
+  color: #fff;
+  display: inline-block;
+  margin-bottom: 5px;
+  padding: 5px 15px;
+  text-decoration: none;
+}
+     p {
+       color: red;
+     }
+   `}</style>
+
+   </div>
+  , document.querySelector('#root'));
